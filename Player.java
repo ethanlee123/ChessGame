b5da300e24eb8408918fddeea268d6c7654ea3cb
Ethan Lee <33e.lee@gmail.com>
2021-10-29 12:54:58 -0700

    add Player class

--- Player.java
+++ Player.java
@@ -1,5 +1,22 @@
 package A00990753;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-  private final int playerNumber;
+  private final ColorSide color;
+  private final List<Piece> pieces;
+
+  Player(ColorSide color) {
+
+    this.color = color;
+    this.pieces = new ArrayList<>();
+  }
+
+  public List<Piece> getPieces() {
+    return this.pieces;
+  }
+  public ColorSide getColor() {
+    return this.color;
+  }
 }
