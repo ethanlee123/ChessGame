7ce16ca4429a28584d3c744773d9091d00ae9966
Ethan Lee <33e.lee@gmail.com>
2021-11-03 20:03:34 -0700

    add out of board boundaries check

--- Board.java
+++ Board.java
@@ -7,6 +7,10 @@ public abstract class Board {
     this.tiles = new Tile[rows][columns];
   }
   public Tile getTile(int row, int column) {
+    if (row < 0 || column < 0 || 
+        row >= tiles.length || column >= tiles[0].length) {
+          return null;
+        }
     return this.tiles[row][column];
   }
   public Tile[][] getTiles() {
