700cb5a8107ed12487987bb410c3a11695e3f36f
Ethan Lee <33e.lee@gmail.com>
2021-11-05 15:20:21 -0700

    add functionality to check valid king movements

--- Chess_Pieces/King.java
+++ Chess_Pieces/King.java
@@ -26,14 +26,33 @@ public class King extends Piece{
   }
 
   @Override
-  public boolean isValidMovement(Board board, Tile fromThisTile, Tile toThisTile) {
-    // TODO Auto-generated method stub
-    return true;
+  public boolean isValidMovement(Board board, Tile fromThisTile, 
+                                 Tile toThisTile) {
+    int[][] possibleValidMoves = this.generateValidMovements(board, fromThisTile);
+
+    if (possibleValidMoves[toThisTile.getRow()][toThisTile.getColumn()] == 1) {
+      return true;
+    }
+    return false;
   }
 
   @Override
   public int[][] generateValidMovements(Board board, Tile fromThisTile) {
-    // TODO Auto-generated method stub
-    return null;
+    int[][] validMoves = 
+      new int[board.getTiles().length][board.getTiles()[0].length];
+    int[][] diagonalCoordinates = {{1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+
+    validVerticalMovementCheck(board, fromThisTile, validMoves, 1);
+    validVerticalMovementCheck(board, fromThisTile, validMoves, -1);
+    validHorizontalMovementCheck(board, fromThisTile, validMoves, 1);
+    validHorizontalMovementCheck(board, fromThisTile, validMoves, -1);
+    
+    for (int i = 0; i < diagonalCoordinates.length; i++) {
+      validDiagonalMovement(board, fromThisTile, validMoves, 
+                            diagonalCoordinates[i][0], 
+                            diagonalCoordinates[i][1]);
+    }
+    
+    return validMoves;
   }
 }
